db6b707465f37d01ea5cff28b9223c8020faca41
OhMyLob <matteolob1704@gmail.com>
2015-09-23 17:54:57 +0200

    Add a working keyboard

--- js/init.js
+++ js/init.js
@@ -2,47 +2,83 @@ var arrayOfValues = new Array();
 
 var numbers = $('.number-item');
 var letters = $(".letter-item");
-var result = $('#result');
-var cancel = $('#cancel');
+var zero    = $('#zero-item');
+var result  = $('#result');
+var cancel  = $('#cancel');
+
+var start;
+var longpress = 400;
 
 function getParagraphTextById(id) {
   return $(id)[0].innerHTML;
 }
 
-function getLastChar(string) {
-    return string.toString().replace(/\d{11}$/, '').slice(0, -1);
+function getStringWithoutLastChar(string) {
+    return string.toString()
+    			.replace(/\d{11}$/, '')
+    			.split(',')
+    			.join('')
+    			.replace(/\s+/g, '');
 }
 
-/*
- * Work in progress
- */
 function getConcatenatedArrayOfValues() {
 	var tmpString;
-	for (var i = 0; i < arrayOfValues.length; i++) {
+	for (var i = 0; i <= arrayOfValues.length; i++) {
 		tmpString.concat(arrayOfValues[i]);
 	}
-	return tmpString.replace(",", "").replace(" ", "");
+	return tmpString;
 }
 
 function removeLastItemFromArrayOfValues() {
-	arrayOfValues.splice(-2,1);
+	arrayOfValues.splice(-1,1);
+}
+
+function clearLastNumber() {
+	removeLastItemFromArrayOfValues();
+	result.text(getStringWithoutLastChar(arrayOfValues.toString()));  
+}
+
+function clearAll() {
+	while(arrayOfValues.length > 0) {
+    	arrayOfValues.pop();
+	}
+	result.text('');
 }
 
 numbers.each(function(index) {
     numbers.eq(index).click(function() {
     	arrayOfValues.push(numbers.eq(index).text());
-		result.text(getParagraphTextById('#result') + numbers.eq(index).text());
+    	var string = getParagraphTextById('#result') + numbers.eq(index).text();
+		result.text(string.replace(/\s+/g, ''));
 	});
 });
 
 letters.each(function(index) {
     letters.eq(index).click(function() {
     	arrayOfValues.push(letters.eq(index).text());
-		result.text(getParagraphTextById('#result') + letters.eq(index).text());
+    	var string = getParagraphTextById('#result') + letters.eq(index).text();
+		result.text(string.replace(/\s+/g, ''));
 	});
 });
 
-cancel.click(function() {
-	removeLastItemFromArrayOfValues();
-	result.text(getLastChar(arrayOfValues.toString()));
+zero.click(function() {
+	arrayOfValues.push(zero.text());
+	var string = getParagraphTextById('#result') + zero.text();
+	result.text(string.replace(/\s+/g, ''));
+});
+
+cancel.on('mousedown', function(e) {
+    start = new Date().getTime();
+});
+
+cancel.on('mouseleave', function(e) {
+    start = 0;
+});
+
+cancel.on('mouseup', function(e) {
+    if (new Date().getTime() >= (start + longpress)) {
+        clearAll();
+    } else {
+        clearLastNumber();
+    }
 });
