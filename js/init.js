14c61de19c265ecb68b195f5208b642435ee3f10
OhMyLob <matteolob1704@gmail.com>
2015-09-30 19:56:48 +0200

    Update JavaScript code

--- js/init.js
+++ js/init.js
@@ -1,5 +1,7 @@
+// Declare array of the input numbers
 var arrayOfValues = new Array();
 
+// Declare needed variables
 var numbers        = $('.number-item');
 var letters        = $('.letter-item');
 var zero           = $('#zero-item');
@@ -11,11 +13,6 @@ var lettersOverlay = $('#hex-letters-overlay');
 var conversion     = $('#conversion-type');
 var simplifyDiv    = $('#simplify');
 
-var conversionType = getParagraphTextById(conversion);
-
-var start;
-var longpress = 400;
-
 var HEXADECIMAL = 'HEX';
 var DECIMAL     = 'DEC';
 var OCTAL       = 'OCT';
@@ -23,20 +20,25 @@ var BINARY      = 'BIN';
 
 var hex, dec, oct, bin;
 
+var conversionType = DECIMAL;
+
+var start;
+var longpress = 400;
+
+// Change this to true if you want some logs
 var debug = true;
 
-function getParagraphTextById(id) {
-  return $(id)[0].innerHTML;
-}
-
+// Check if the input number is binary
 function isBinary(input) {
   return input.search(/^[10]+$/) != -1;
 }
 
+// Check if the input number is octal
 function isOctal(input) {
   return input.search(/^[01234567]+$/) != -1;
 }
 
+// Remove last char of the string
 function getStringWithoutLastChar(string) {
     return string.toString()
     			.replace(/\d{11}$/, '')
@@ -45,6 +47,7 @@ function getStringWithoutLastChar(string) {
     			.replace(/\s+/g, '');
 }
 
+// This returns a string of concatenated values stored inside an array
 function getConcatenatedArrayOfValues() {
 	var tmpString;
 	for (var i = 0; i < arrayOfValues.length; i++) {
@@ -53,10 +56,12 @@ function getConcatenatedArrayOfValues() {
 	return tmpString;
 }
 
+// Remove the last item from an array
 function removeLastItemFromArrayOfValues() {
 	arrayOfValues.splice(-1, 1);
 }
 
+// Set output clearing the last element
 function clearLastNumber() {
 	removeLastItemFromArrayOfValues();
   var string = getStringWithoutLastChar(arrayOfValues.toString());
@@ -69,6 +74,7 @@ function clearLastNumber() {
   }
 }
 
+// Remove every item inside an array
 function clearAll() {
 	while(arrayOfValues.length > 0) {
     	arrayOfValues.pop();
@@ -77,6 +83,7 @@ function clearAll() {
   output.text('');
 }
 
+// Calculate the values [base = 16]
 function calculateFromHex(input) {
   hex = parseInt(input).toString().toUpperCase();
   dec = parseInt(input, 16).toString(10);
@@ -84,6 +91,7 @@ function calculateFromHex(input) {
   bin = parseInt(input, 16).toString(2);
 }
 
+// Calculate the values [base = 10]
 function calculateFromDec(input) {
   hex = parseInt(input, 10).toString(16).toUpperCase();
   dec = parseInt(input, 10).toString();
@@ -91,6 +99,7 @@ function calculateFromDec(input) {
   bin = parseInt(input, 10).toString(2);
 }
 
+// Calculate the values [base = 8]
 function calculateFromOct(input) {
   hex = parseInt(input, 8).toString(16).toUpperCase();
   dec = parseInt(input, 8).toString(10);
@@ -98,6 +107,7 @@ function calculateFromOct(input) {
   bin = parseInt(input, 8).toString(2);
 }
 
+// Calculate the values [base = 2]
 function calculateFromBin(input) {
   hex = parseInt(input, 2).toString(16).toUpperCase();
   dec = parseInt(input, 2).toString(10);
@@ -105,6 +115,7 @@ function calculateFromBin(input) {
   bin = parseInt(input).toString();
 }
 
+// Switch the conversion type
 function changeConversion(input) {
   if (conversionType == HEXADECIMAL) {
     conversionType = DECIMAL;
@@ -121,6 +132,7 @@ function changeConversion(input) {
   }
 }
 
+// The main function used to show the output
 function calculate(mustChangeConversion) {
   var input = result.text();
 
@@ -173,20 +185,24 @@ function calculate(mustChangeConversion) {
   }
 }
 
+// Setup tooltips
 $(document).ready(function() {
   Tipped.create('#conversion-type', 'Change base.');
   Tipped.create('#arrow-advanced', 'Show letters.');
 });
 
+// Hide the letters and the arrow down
 lettersDiv.hide();
 simplifyDiv.hide();
 
+// Handle arrow up click
 lettersOverlay.click(function() {
   $(this).hide();
   lettersDiv.show();
   simplifyDiv.show();
 });
 
+// Handle arrow down click
 simplifyDiv.click(function() {
   $(this).hide();
   lettersDiv.hide();
@@ -194,35 +210,40 @@ simplifyDiv.click(function() {
   lettersOverlay.show();
 });
 
+// Handle EACH number click
 numbers.each(function(index) {
     numbers.eq(index).click(function() {
     	arrayOfValues.push(numbers.eq(index).text());
-    	var string = getParagraphTextById('#result') + numbers.eq(index).text();
+    	var string = result.text() + numbers.eq(index).text();
 		  result.text(string.replace(/\s+/g, ''));
       calculate(false);
 	});
 });
 
+// Handle EACH letter click
 letters.each(function(index) {
     letters.eq(index).click(function() {
     	arrayOfValues.push(letters.eq(index).text());
-    	var string = getParagraphTextById('#result') + letters.eq(index).text();
+    	var string = result.text() + letters.eq(index).text();
 		  result.text(string.replace(/\s+/g, ''));
       calculate(false);
 	});
 });
 
+// Handle 0 click
 zero.click(function() {
 	arrayOfValues.push(zero.text());
-	var string = getParagraphTextById('#result') + zero.text();
+	var string = result.text() + zero.text();
 	result.text(string.replace(/\s+/g, ''));
   calculate(false);
 });
 
+// Handle conversion [BIN/OCT/DEC/HEX]
 conversion.click(function() {
   calculate(true);
 });
 
+// Handle longpress to clear all
 cancel.on('mousedown', function(e) {
     start = new Date().getTime();
 });
